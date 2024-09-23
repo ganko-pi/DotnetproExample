e94f1d516d77e49774147ad8f7444bf716ec44ec
Ganko Pi <ganko-pi@outlook.de>
2024-09-23 11:59:17 +0200

    feat: add rectangular texture with a random color for each pixel

--- DotnetproExample/Game1.cs
+++ DotnetproExample/Game1.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Linq;
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+
+using MonoGame.Extended;
+using MonoGame.Extended.ViewportAdapters;
 
 namespace DotnetproExample;
 
@@ -8,6 +14,9 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private OrthographicCamera _camera;
+
+    private Texture2D _playerSprite;
 
     public Game1()
     {
@@ -19,6 +28,8 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        ViewportAdapter viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, 320, 180);
+        _camera = new OrthographicCamera(viewportAdapter);
 
         base.Initialize();
     }
@@ -26,8 +37,9 @@ public class Game1 : Game
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+        
         // TODO: use this.Content to load your game content here
+        _playerSprite = GenerateRandomTexture(GraphicsDevice, 16, 16);
     }
 
     protected override void Update(GameTime gameTime)
@@ -45,7 +57,22 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         // TODO: Add your drawing code here
+        Matrix transformMatrix = _camera.GetViewMatrix();
+        _spriteBatch.Begin(transformMatrix: transformMatrix, samplerState: SamplerState.PointClamp);
+        _spriteBatch.Draw(_playerSprite, new Vector2(50, 50), Color.White);
+        _spriteBatch.End();
 
         base.Draw(gameTime);
+    }
+
+    private Texture2D GenerateRandomTexture(GraphicsDevice graphicsDevice, int width, int height)
+    {
+        Random random = new();
+        Color[] data = Enumerable.Range(0, width * height)
+            .Select(_ => new Color(random.Next(256), random.Next(256), random.Next(256)))
+            .ToArray();
+        Texture2D texture = new(graphicsDevice, width, height);
+        texture.SetData(data);
+        return texture;
     }
 }
--- DotnetproExample/Program.cs
+++ DotnetproExample/Program.cs
@@ -1,3 +1,2 @@
-﻿
-using var game = new DotnetproExample.Game1();
+﻿using var game = new DotnetproExample.Game1();
 game.Run();
